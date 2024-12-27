99384ed72c6410bfa5a46e6e2df1025f00640027
Raylan <raylan.sales@unyleya.com.br>
2024-12-27 16:07:29 -0300

    api install and creating items routes

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\ItemController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('/items', [ItemController::class, 'index']);
+Route::prefix('/item')->group(function () {
+    Route::post('/', [ItemController::class, 'store']);
+    Route::put('/{item}', [ItemController::class, 'update']);
+    Route::delete('/{item}', [ItemController::class, 'destroy']);
+});
